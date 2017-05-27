8b2e78c2ed3281bdd396f81bff90a3a10e4c9574
Chris <challer6@gmail.com>
2017-05-27 15:03:53 -0700

    Complete Sunshine Exercise S01.02

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,10 +17,13 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    // Holds the weather information
+    TextView weather_display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         // TODO (2) Use findViewById to get a reference to the weather display TextView
+        weather_display = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (3) Create an array of Strings that contain fake weather data
+        String[] weather_data = {
+                "May 27 - Sunny and Clear - 86°F / 54°F",
+                "May 28 - Sunny and Clear - 84°F / 53°F",
+                "May 29 - Sunny and Clear - 80°F / 54°F",
+                "May 30 - Sunny and Clear - 70°F / 54°F",
+                "May 31 - Sunny and Clear - 69°F / 51°F",
+                "June 01 - Sunny and Clear - 72°F / 52°F",
+                "June 02 - Sunny and Clear - 71°F / 50°F",
+                "June 03 - Sunny and Clear - 72°F / 49°F",
+                "June 04 - Sunny and Clear - 72°F / 50°F",
+                "June 05 - Sunny and Clear - 77°F / 50°F"
+        };
 
         // TODO (4) Append each String from the fake weather data array to the TextView
+        for (String ind_data: weather_data) {
+            weather_display.append(ind_data +"\n\n\n");
+        }
     }
 }
