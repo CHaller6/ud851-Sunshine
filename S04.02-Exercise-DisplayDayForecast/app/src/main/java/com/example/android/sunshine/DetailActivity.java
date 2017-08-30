a480237fdd2461547c7730fe7d13e3c373d54b5e
Chris <challer6@gmail.com>
2017-08-29 19:37:55 -0700

    Complete Exercise S04.02

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,6 +14,23 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        // COMPLETE (3): Retrieve the TextView to which the name of the item clicked should be displayed.
+        TextView detailNameTV = (TextView) findViewById(R.id.detail_name);
+
+        // COMPLETE (2) Display the weather forecast that was passed from MainActivity
+        // COMPLETE (4) Retrieve the intent that started this activity.
+        Intent intentThatStartedThis = getIntent();
+
+        // COMPLETE (5) Check to see if the Intent that started this activity exists
+        if (intentThatStartedThis != null) {
+            // COMPLETE (6) Check to see if Intent had any extra data attached to it using hasExtra() and Intent.EXTRA_TEXT
+            if (intentThatStartedThis.hasExtra(Intent.EXTRA_TEXT) == true) {
+                // COMPLETE (7) If the intent has some extra data, retrieve it and place it in a String variable.
+                String detailName = intentThatStartedThis.getStringExtra(Intent.EXTRA_TEXT);
+                // COMPLETE (8) If the intent has some extra data, set the TextView to display it.
+                detailNameTV.setText(detailName);
+            }
+
+        }
     }
 }
